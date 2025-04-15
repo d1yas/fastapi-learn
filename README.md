1dc3c60689dbb6ebd036a721a5bf0625968bbee0
d1yas <diyasdjumabaevv@gmail.com>
2025-04-15 13:10:39 +0500

    get api endpoint

--- README.md
+++ README.md
@@ -5,4 +5,8 @@ or
 ```pip install 'fastapi[standart]'```
 
 
+RUN fastapi App
 
+```fastapi dev main.py```
+or
+```uvicorn main:app --reload```
--- main.py
+++ main.py
@@ -1,7 +1,38 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, HTTPException
+import uvicorn
 app = FastAPI()
 
-@app.get('/')
+@app.get('/',summary='Home', tags=['Get'])
 def root():
     return 'Hello world'
+
+
+books = [
+    {
+        'id': 1,
+        'title': 'Roadmap Django',
+        'author': 'roadmap.sh',
+    },
+    {
+        'id': 2,
+        'title': 'Roadmap FastAPI',
+        'author': 'solvit',
+    }
+]
+
+
+@app.get('/books',summary='Books', tags=['Get'])
+def read_books():
+    return books
+
+@app.get('/books/{id}',summary='Book', tags=['Get'])
+def get_book(id: int):
+    for book in books:
+        if book['id'] == id:
+            return book
+    raise HTTPException(status_code=404, detail='Book not found')
+
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app",reload=True)
