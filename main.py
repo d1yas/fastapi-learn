7864fd87d1fcee57264126f4d6be205f1a1f45bd
d1yas <diyasdjumabaevv@gmail.com>
2025-04-15 13:27:25 +0500

    post api endpoint

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI, HTTPException
 import uvicorn
+from pydantic import BaseModel
+
 app = FastAPI()
 
-@app.get('/',summary='Home', tags=['Get'])
-def root():
-    return 'Hello world'
+# @app.get('/',summary='Home', tags=['Get'])
+# def root():
+#     return 'Hello world'
 
 
 books = [
@@ -21,11 +23,11 @@ books = [
 ]
 
 
-@app.get('/books',summary='Books', tags=['Get'])
+@app.get('/books',summary='Get all books', tags=['Get'])
 def read_books():
     return books
 
-@app.get('/books/{id}',summary='Book', tags=['Get'])
+@app.get('/books/{id}',summary='Get book', tags=['Get'])
 def get_book(id: int):
     for book in books:
         if book['id'] == id:
@@ -33,6 +35,21 @@ def get_book(id: int):
     raise HTTPException(status_code=404, detail='Book not found')
 
 
+
+class NewBook(BaseModel):
+    title: str
+    author: str
+
+@app.post('/books/', tags=['Post'])
+def create_book(new_book: NewBook):
+    books.append({
+        "id": len(books) + 1,
+        "title": new_book.title,
+        "author": new_book.author,
+    })
+
+    return {'success': 201, "message": "Book created successfully"}
+    # return new_book
 
 if __name__ == '__main__':
     uvicorn.run("main:app",reload=True)
